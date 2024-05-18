408209801c95244cd7be13d7d0a695468d410637
efrainjmg <efrain.martinez@unicolombo.edu.co>
2024-05-17 21:36:12 -0500

    se añadió la clase infraccion, el contralador de infraccion, el repositorio de infraccion y el servicio de infraccion

---
+++ src/main/java/com/gestor/infracciones/gestorinfracciones/controller/InfraccionController.java
@@ -0,0 +1,45 @@
+package com.gestor.infracciones.gestorinfracciones.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gestor.infracciones.gestorinfracciones.entity.Infraccion;
+import com.gestor.infracciones.gestorinfracciones.service.InfraccionService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+public class InfraccionController {
+    private InfraccionService service;
+
+    @Autowired
+    @PostMapping("/addInfraccion")
+    public Infraccion addInfraccion(@RequestBody Infraccion infraccion) {
+        return service.saveInfraccion(infraccion);
+    }
+    
+    @GetMapping("/infracciones")
+    public List<Infraccion> findInfracciones() {
+        return service.getInfraccion();
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public void deleteInfraccion(@PathVariable int id){
+        service.deleteInfraccion(id);
+    }
+
+    @PutMapping("updateInfraccion/{id}")
+    public Infraccion updateInfraccion(@PathVariable int id, @RequestBody Infraccion infraccion) {
+        
+        return service.updateInfraccion(infraccion);
+    }
+}
