227293fbb921babc9f9dc9db51f91da492d350d0
rdavidgonzaleza <rdavidgonzaleza@outlook.com>
2024-05-22 01:00:17 -0500

    se eliminaron los controllers de persona referentes a create y a delete Persona

--- src/main/java/com/gestor/infracciones/gestorinfracciones/controller/PersonaController.java
+++ src/main/java/com/gestor/infracciones/gestorinfracciones/controller/PersonaController.java
@@ -2,15 +2,12 @@ package com.gestor.infracciones.gestorinfracciones.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gestor.infracciones.gestorinfracciones.entity.Persona;
 import com.gestor.infracciones.gestorinfracciones.service.PersonaService;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,21 +18,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RestController
 public class PersonaController {
     private PersonaService service;
-
-    @Autowired
-    @PostMapping("/addPersona")
-    public Persona addPersona(@RequestBody Persona persona) {
-        return service.savePersona(persona);
-    }
     
     @GetMapping("/personas")
     public List<Persona> findPersonas() {
         return service.getPersona();
-    }
-    
-    @DeleteMapping("/delete/{id}")
-    public void deletePersona(@PathVariable int id){
-        service.deletePersona(id);
     }
 
     @PutMapping("updatePersona/{id}")
